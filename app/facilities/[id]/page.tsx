a2d5a1a65fdd81333e896bbd9fe136f0923ccb6a
Mingwei100 <mingwei100@gmail.com>
2025-01-10 19:08:46 -0800

    Update page.tsx

--- app/facilities/[id]/page.tsx
+++ app/facilities/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { FacilityDetailsComponent } from '@/components/facilities/1/FacilityDetails';
 import { facilities } from '@/components/facilities';
 import sampleImage from '@/assets/sample_rcfe.jpeg'
-import localImage1 from '@/assets/facility_1_1.JPG'
-import localImage2 from '@/assets/facility_1_2.JPG'
-import localImage3 from '@/assets/facility_1_3.JPG'
-import localImage4 from '@/assets/facility_1_4.JPG'
-import localImage5 from '@/assets/facility_1_5.JPG'
+import localImage1 from '@/assets/facility_1_1.jpg'
+import localImage2 from '@/assets/facility_1_2.jpg'
+import localImage3 from '@/assets/facility_1_3.jpg'
+import localImage4 from '@/assets/facility_1_4.jpg'
+import localImage5 from '@/assets/facility_1_5.jpg'
 
 export default function FacilityDetailsPage({ params }: { params: { id: string } }) {
   const facility = facilities.find(f => f.id === parseInt(params.id));
