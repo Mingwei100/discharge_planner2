0b99c7c5c3a66502576524d6b8ead9cecd1c3008
Mingwei100 <mingwei100@gmail.com>
2025-01-10 17:01:56 -0800

    fix jpg capitalization

--- app/facilities/[id]/page.tsx
+++ app/facilities/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { FacilityDetailsComponent } from '@/components/facilities/1/FacilityDetails';
 import { facilities } from '@/components/facilities';
 import sampleImage from '@/assets/sample_rcfe.jpeg'
+import localImage1 from '@/assets/facility_1_1.JPG'
+import localImage2 from '@/assets/facility_1_2.JPG'
+import localImage2 from '@/assets/facility_1_3.JPG'
+import localImage2 from '@/assets/facility_1_4.JPG'
+import localImage2 from '@/assets/facility_1_5.JPG'
+
 export default function FacilityDetailsPage({ params }: { params: { id: string } }) {
   const facility = facilities.find(f => f.id === parseInt(params.id));
   return (
@@ -12,7 +18,7 @@ export default function FacilityDetailsPage({ params }: { params: { id: string }
       topAttributes={facility.topAttributes}
       description= {facility.description}
       price={facility.price}
-      images={[sampleImage.src]}
+      images={[sampleIage.src,localImage1.src]}
     />
   );
 }
