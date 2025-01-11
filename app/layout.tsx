4979dea4ec0bf9ed25324929f966dae298b5a415
Mingwei100 <mingwei100@gmail.com>
2025-01-10 20:34:51 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <LayoutContent>
           {children}
+          <Analytics />
         </LayoutContent>
         <Toaster />
       </body>
