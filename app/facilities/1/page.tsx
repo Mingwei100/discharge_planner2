2d40b460f1ee79c90f46b1a4bdbdf08ce88130b6
Mingwei100 <mingwei100@gmail.com>
2025-01-11 23:28:32 -0800

    Update page.tsx

--- app/facilities/1/page.tsx
+++ app/facilities/1/page.tsx
@@ -1,5 +1,11 @@
 import { FacilityDetailsComponent } from '@/components/facilities/1/FacilityDetails';
 import { facilities } from '@/components/facilities';
+import sampleImage from '@/assets/facility_1_1.jpg'
+import sampleImage2 from '@/assets/facility_1_2.jpg'
+import sampleImage3 from '@/assets/facility_1_3.jpg'
+import sampleImage4 from '@/assets/facility_1_4.jpg'
+import sampleImage5 from '@/assets/facility_1_5.jpg'
+
 const topAttributes = [
   "24/7 Care",
   "Home-cooked Meals",
@@ -17,7 +23,7 @@ export default function FacilityDetailsPage({ params }: { params: { id: string }
       topAttributes={topAttributes}
       description="A warm and welcoming care facility..."
       price={5000}
-      images={[/* array of image URLs */]}
+      images={[sampleImage.src,sampleImage2.src,sampleImage3.src,sampleImage4.src,sampleImage5.src]}
     />
   );
 }
